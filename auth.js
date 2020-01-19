07c104f8c5b59b38ad827b20c2653be5aae3cd8f
fakhrads <fakhriadisaputra15@gmail.com>
2020-01-19 09:56:48 +0700

    Update

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 module.exports = {
-    consumer_key: process.env.C_KEY,
-    consumer_secret: process.env.C_SECRET,
-    access_token: process.env.A_TOKEN,
-    access_token_secret: process.env.A_SECRET,
+    consumer_key: 'A1pNddbfdSxhMhsm4Le2L6klU',
+    consumer_secret: 'vMmWGWwGPEXX9ck694t3zwfF8zn0lH8uAA22eT9F6JQu5SHfBy',
+    access_token: '1218508377094946816-dMDsvxTE09epzdq0jNnj81aA2XDM7T',
+    access_token_secret: 'BLFG5iL6Kv4ojiKmGye0J6q4WHd78y4gzep4Ajpot3Wkp',
     timeout_ms: 60*1000
- }
+}
